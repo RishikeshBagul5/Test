171913c59810b813eb47200ffa97c4528a8eabe6
RishikeshBagul5 <rushikeshbagul.01@gmail.com>
2025-01-29 00:36:44 +0530

    Rectangle

--- Area_Triangle.py
+++ Area_Triangle.py
@@ -1,6 +1,7 @@
 def Area_of_Tri(base,height):
     Area = 1/2*(base*height)
     print("Area of triangle is ",Area)
+    print("Area of React")
 
 if __name__ == "__main__":
     a = int(input("Enter base dimension "))
