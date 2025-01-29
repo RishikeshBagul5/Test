80b01c46c72bf05bb0b96c3f75190430dcd4fc64
RishikeshBagul5 <rushikeshbagul.01@gmail.com>
2025-01-29 23:40:13 +0530

    Hexagon

--- Area_Triangle.py
+++ Area_Triangle.py
@@ -3,6 +3,7 @@ def Area_of_Tri(base,height):
     print("Area of triangle is ",Area)
     print("Area of React")
     print("Area of Square")
+    print("Area of Hexagon")
 
 if __name__ == "__main__":
     a = int(input("Enter base dimension "))
