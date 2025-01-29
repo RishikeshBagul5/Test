f7d24afc911c2ce56c1f3906e4718045b5d0bf5b
RishikeshBagul5 <rushikeshbagul.01@gmail.com>
2025-01-29 23:32:06 +0530

    Nika

--- Area_Triangle.py
+++ Area_Triangle.py
@@ -3,7 +3,7 @@ def Area_of_Tri(base,height):
     print("Area of triangle is ",Area)
     print("Area of React")
     print("Area of Square")
-
+    print("God of the Pirates")
 if __name__ == "__main__":
     a = int(input("Enter base dimension "))
     b = int(input("Enter Height Dimension "))
